1b26886dc7ebf5649b35b4628ff8282bf252e808
FatemeZahraAzad <mahilda.azad@gmail.com>
2024-05-03 15:14:04 +0330

    overfitting test

--- FirstMachineLearningExercise.py
+++ FirstMachineLearningExercise.py
@@ -34,4 +34,90 @@ model = KNeighborsRegressor(n_neighbors=3)
 model.fit(x, y)
 
 X_new = [[37_655.2]]
-print(model.predict(X_new))
+# print(model.predict(X_new))
+IMAGES_PATH = Path()
+IMAGES_PATH.mkdir(exist_ok=True, parents=True)
+
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+datapath = Path() / "datasets" / "lifesat"
+datapath.mkdir(parents=True, exist_ok=True)
+
+data_root = "https://github.com/ageron/data/raw/main/"
+for filename in ("oecd_bli.csv", "gdp_per_capita.csv"):
+    if not (datapath / filename).is_file():
+        print("Downloading", filename)
+        url = data_root + "lifesat/" + filename
+        urllib.request.urlretrieve(url, datapath / filename)
+
+oecd_bli = pd.read_csv(datapath / "oecd_bli.csv")
+gdp_per_capita = pd.read_csv(datapath / "gdp_per_capita.csv")
+
+gdp_year = 2020
+gdppc_col = "GDP per capita (USD)"
+lifesat_col = "Life satisfaction"
+
+gdp_per_capita = gdp_per_capita[gdp_per_capita["Year"] == gdp_year]
+gdp_per_capita = gdp_per_capita.drop(["Code", "Year"], axis=1)
+gdp_per_capita.columns = ["Country", gdppc_col]
+gdp_per_capita.set_index("Country", inplace=True)
+
+# print(gdp_per_capita.head())
+
+oecd_bli = oecd_bli[oecd_bli["INEQUALITY"] == "TOT"]
+oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+
+# print(oecd_bli.head())
+
+full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
+                              left_index=True, right_index=True)
+full_country_stats.sort_values(by=gdppc_col, inplace=True)
+full_country_stats = full_country_stats[[gdppc_col, lifesat_col]]
+
+# print(full_country_stats.head())
+
+min_gdp = 23_500
+max_gdp = 62_500
+
+country_stats = full_country_stats[(full_country_stats[gdppc_col] >= min_gdp) &
+                                   (full_country_stats[gdppc_col] <= max_gdp)]
+# print(country_stats.head())
+country_stats.to_csv(datapath / "lifesat.csv")
+full_country_stats.to_csv(datapath / "lifesat_full.csv")
+
+country_stats.plot(kind='scatter', figsize=(5, 3), grid=True,
+                   x=gdppc_col, y=lifesat_col)
+
+min_life_sat = 4
+max_life_sat = 9
+
+position_text = {
+    "Turkey": (29_500, 4.2),
+    "Hungary": (28_000, 6.9),
+    "France": (40_000, 5),
+    "New Zealand": (28_000, 8.2),
+    "Australia": (50_000, 5.5),
+    "United States": (59_000, 5.3),
+    "Denmark": (46_000, 8.5)
+}
+
+for country, pos_text in position_text.items():
+    pos_data_x = country_stats[gdppc_col].loc[country]
+    pos_data_y = country_stats[lifesat_col].loc[country]
+    country = "U.S." if country == "United States" else country
+    plt.annotate(country, xy=(pos_data_x, pos_data_y),
+                 xytext=pos_text, fontsize=12,
+                 arrowprops=dict(facecolor='black', width=0.5,
+                                 shrink=0.08, headwidth=5))
+    plt.plot(pos_data_x, pos_data_y, "ro")
+
+plt.axis([min_gdp, max_gdp, min_life_sat, max_life_sat])
+
+save_fig('money_happy_scatterplot')
+plt.show()
